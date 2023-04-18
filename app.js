fa21bc8e0ae2f5bef9fe3940f73d4868c16e82f5
Mohamed Hamza KERMIA <54137287+kermia-ai@users.noreply.github.com>
2023-04-18 16:26:49 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,13 @@ document.getElementById("simulation-form").addEventListener("submit", function (
 
     // Get values from the input fields
     const annualEnergyConsumptionKwh = parseFloat(document.getElementById("annual-energy-consumption-kwh").value);
-    // Add more variables for other input fields
+    const coverageRatio = parseFloat(document.getElementById("coverage-ratio").value);
+    const solarIrradiation = parseFloat(document.getElementById("solar-irradiation").value);
+    const roofOrientation = parseFloat(document.getElementById("roof-orientation").value);
+    const systemEfficiency = parseFloat(document.getElementById("system-efficiency").value);
+    const panelEfficiency = parseFloat(document.getElementById("panel-efficiency").value);
+    const inverterEfficiency = parseFloat(document.getElementById("inverter-efficiency").value);
+    const desiredStorageCapacityHours = parseFloat(document.getElementById("desired-storage-capacity-hours").value);
 
     const [panelArea, storageCapacity] = pvSystemDimensioning(
         annualEnergyConsumptionKwh,
@@ -18,6 +24,7 @@ document.getElementById("simulation-form").addEventListener("submit", function (
 
     // Display results
     document.getElementById("results").innerHTML = `
+        <h2>Résultats:</h2>
         <p>Surface des panneaux solaires (m²): ${panelArea.toFixed(2)}</p>
         <p>Capacité de stockage optimale (kWh): ${storageCapacity.toFixed(2)}</p>
     `;
@@ -33,5 +40,12 @@ function pvSystemDimensioning(
     inverterEfficiency,
     desiredStorageCapacityHours
 ) {
-    // ... Add the same pvSystemDimensioning logic as in the Python code
+    const dailyEnergyConsumptionKwh = annualEnergyConsumptionKwh / 365;
+    const dailyEnergyProductionKwh = dailyEnergyConsumptionKwh * coverageRatio / 100;
+    const effectiveIrradiation = solarIrradiation * Math.cos(Math.PI * roofOrientation / 180);
+    const panelOutputKwhPerSquareMeter = effectiveIrradiation * panelEfficiency / 100;
+    const panelArea = dailyEnergyProductionKwh / panelOutputKwhPerSquareMeter;
+    const storageCapacity = dailyEnergyConsumptionKwh * desiredStorageCapacityHours * inverterEfficiency / (100 * systemEfficiency / 100);
+
+    return [panelArea, storageCapacity];
 }
